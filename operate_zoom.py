19b551f87700ee807bf746d859a13536342b4e07
mba_ateruimashin <ateruimashin2go@gmail.com>
2021-02-04 16:52:09 +0900

    Test moving mouse

--- operate_zoom.py
+++ operate_zoom.py
@@ -1,2 +1,16 @@
-print("Hello,World")
-print("Test")
+#! python3
+import pyautogui, sys
+print('Press Ctrl-C to quit.')
+try:
+    pyautogui.moveTo(10,10)
+    while True:
+        pyautogui.move(50,50,0.5)
+        x,y=pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+
+        if y >= 700:
+            pyautogui.moveTo(0,0)
+except KeyboardInterrupt:
+    print('\n')
