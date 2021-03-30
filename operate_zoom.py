2f5cec337c67fa7a55f1b0d3f3be3439d1c797c5
collagePC <ateruimashin2go@gmail.com>
2021-03-30 20:32:31 +0900

    微調整

--- operate_zoom.py
+++ operate_zoom.py
@@ -1,10 +1,13 @@
 import pyautogui as pg
 import sys
+import os
 
-#フェールセーフショートカットキー
+os.environ['DISPLAY'] = ':0'
+
+# フェールセーフショートカットキー
 print('Press Ctrl-C to quit.')
 
-#ズーム起動部分(windows専用)
+# ズーム起動部分(windows専用)
 def run_zoom():
 
     pg.press('win')
@@ -13,50 +16,51 @@ def run_zoom():
     pg.PAUSE = 2
     pg.press('enter')
 
-#定期ミーティング起動部分
+# 定期ミーティング起動部分
 def start_meeting():
 
-    #ボタンがアクティブ\非アクティブ状態両方のスクリーンショットをそれぞれリストに入れる。スタートは変数で持つ。
-    buttton_active = [r'button\active\zoom_meeting_active.PNG',r'button\active\zoom_igrashi_meeting_active.PNG']
-    button_nonactive = [r'button\nonactive\zoom_meeting_non.PNG',r'button\nonactive\zoom_igarashi_meeting_non.PNG']
+    # ボタンがアクティブ\非アクティブ状態両方のスクリーンショットをそれぞれリストに入れる。スタートは変数で持つ。
+    buttton_active = [r'button\active\zoom_meeting_active.PNG',
+                      r'button\active\zoom_igrashi_meeting_active.PNG']
+    button_nonactive = [r'button\nonactive\zoom_meeting_non.PNG',
+                        r'button\nonactive\zoom_igarashi_meeting_non.PNG']
     button_start = r'button\active\zoom_igarashi-meeting_start.PNG'
 
-    #画像認識部分
+    # 画像認識部分
     for button_a, button_n in zip(buttton_active, button_nonactive):
 
-        #非アクティブ時のボタンを探す
+        # 非アクティブ時のボタンを探す
         button_loc_n = pg.locateOnScreen(button_n)
 
         if button_loc_n is None:
-            #非アクティブ時のボタンが見つからない場合、アクティブ時のボタンを探す
+            # 非アクティブ時のボタンが見つからない場合、アクティブ時のボタンを探す
             button_loc_a = pg.locateOnScreen(button_a)
 
             if button_loc_a is None:
-                #ボタンが見つからないならばプログラムを終了する
+                # ボタンが見つからないならばプログラムを終了する
                 sys.exit()
             else:
-                #ボタンがアクティブならば操作は必要ない
+                # ボタンがアクティブならば操作は必要ない
                 continue
 
-        #公式ドキュメントのサンプルコードを参考に実装
-        #ボタンの中心の位置の座標を取得しクリックする
+        # 公式ドキュメントのサンプルコードを参考に実装
+        # ボタンの中心の位置の座標を取得しクリックする
         button_center = pg.center(button_loc_n)
         button_x, button_y = button_center
-        pg.click(button_x,button_y)
+        pg.click(button_x, button_y)
 
-        #画面遷移のために1秒停止する
+        # 画面遷移のために1秒停止する
         pg.PAUSE = 1
 
-    #ミーティング開始ボタンを押す部分    
+    # ミーティング開始ボタンを押す部分
     button_x, button_y = pg.center(pg.locateOnScreen(button_start))
-    pg.click(button_x,button_y)
-    
-#メイン部分
+    pg.click(button_x, button_y)
+
+
+# メイン部分
 try:
-    while True:
-        run_zoom()
-        pg.PAUSE = 1
-        start_meeting()
-        break
+    run_zoom()
+    pg.PAUSE = 5 #Zoomの起動待ち
+    start_meeting()
 except KeyboardInterrupt:
     print('\n')
